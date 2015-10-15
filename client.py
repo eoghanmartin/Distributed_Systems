989b95cf51da6223196a4a8a207bb5541c09e295
eoghanmartin <martine4@tcd.ie>
2015-10-15 20:39:54 +0100

    thread pool

--- client.py
+++ client.py
@@ -1,28 +1,30 @@
 # Echo client program
 import socket
 import sys
+import time
 
 HOST = 'localhost'
 PORT = 8000
 s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        s = None
-        continue
-    try:
-        s.connect(sa)
-    except socket.error as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print 'could not open socket'
-    sys.exit(1)
-s.sendall('GET /echo.php?message=caps HTTP/1.1\r\n\r\n\r\n')
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+while 1:
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except socket.error as msg:
+            s = None
+            continue
+        try:
+            s.connect(sa)
+        except socket.error as msg:
+            s.close()
+            s = None
+            continue
+        break
+    if s is None:
+        print 'could not open socket'
+        sys.exit(1)
+    s.sendall('HELO text\n')
+    data = s.recv(1024)
+    s.close()
+    print 'Received', repr(data)
--- server.py
+++ server.py
@@ -1,26 +1,68 @@
-from socket import AF_INET, SOCK_STREAM, socket
-from concurrent.futures
+from SocketServer import ThreadingMixIn
+from Queue import Queue
+import threading, socket
+import time
 
-def echo_client(sock, client_addr):
-    '''
-    Handle a client connection
-    '''
-    print('Got connection from', client_addr)
-    while True:
-        msg = sock.recv(65536)
-        if not msg:
-            break
-        sock.sendall(msg)
-    print('Client closed connection')
-    sock.close()
 
-def echo_server(addr):
-    pool = concurrent.futures.ThreadPoolExecutor(128)
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.bind(addr)
-    sock.listen(5)
-    while True:
-        client_sock, client_addr = sock.accept()
-        pool.submit(echo_client, client_sock, client_addr)
+class ThreadPoolMixIn(ThreadingMixIn):
+    numThreads = 10
+    allow_reuse_address = True
 
-echo_server(('',8000))
+    def serve_forever(self):
+        # set up the threadpool
+        self.requests = Queue(self.numThreads)
+
+        for x in range(self.numThreads):
+            t = threading.Thread(target = self.process_request_thread)
+            t.setDaemon(1) # makes thread a Daemon thread
+            t.start()
+
+        # server main loop
+        while True:
+            self.handle_request()
+        self.server_close()
+
+    
+    def process_request_thread(self):
+        while True:
+            ThreadingMixIn.process_request_thread(self, *self.requests.get())
+
+    
+    def handle_request(self):
+        # add requests to the queue for the workers
+        try:
+            request, client_address = self.get_request()
+        except socket.error:
+            return
+        self.requests.put((request, client_address))
+        data = request.recv(1024)
+        time.sleep(4)
+        if data == "HELO text\n":
+            request.sendall("HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n")
+        print threading.current_thread()
+        if not data: return
+        print data
+        print 'data returned'
+
+if __name__ == '__main__':
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from SocketServer import TCPServer
+    
+    class ThreadedServer(ThreadPoolMixIn, TCPServer):
+        pass
+
+    def echo(HandlerClass = SimpleHTTPRequestHandler,
+            ServerClass = ThreadedServer, 
+            protocol="HTTP/1.1"):
+
+        port = 8000
+        server_address = ('localhost', port)
+
+        HandlerClass.protocol_version = protocol
+        conn = ServerClass(server_address, HandlerClass)
+
+        sa = conn.socket.getsockname()
+        print "Serving connection on", sa[0], "port", sa[1], "..."
+        conn.serve_forever()
+
+    echo()
