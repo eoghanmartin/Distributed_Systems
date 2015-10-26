9f9820265aa726ec2db2c75c03d2005f0c3ebc8e
eoghanmartin <martine4@tcd.ie>
2015-10-26 17:09:40 +0000

    Imports

--- Lab 2/server.py
+++ Lab 2/server.py
@@ -1,33 +1,34 @@
-if __name__ == '__main__':
-    import random
-    import time
-    import socket
-    import sys
-    execfile("threadpool.py")
+import threading
+from threadpool import makeRequests, ThreadPool
+import time
+import socket
+import sys
 
-    exit = 0
+exit = 0
+
+if __name__ == '__main__':
+    #exec(open("threadpool.py").read())
 
     def use_client(data):
         global exit
-        print threading.current_thread()
+        print (threading.current_thread())
         while 1:
             time.sleep(1)
             client, address = s.accept()
             data = client.recv(size)
             if "KILL_SERVICE" in data:
-                print "closing..."
+                print ("closing...")
                 exit = 1
             if "HELO" in data:
-                print(data +"IP: " + str(address) + "\nPort: " + str(port) + "\nStudentID: " + studentID + "\n")
+                print(data + "IP: " + socket.gethostbyname(socket.gethostname()) + "\nPort: " + str(port) + "\nStudentID: " + studentID + "\n")
                 client.send(data + "IP: " + socket.gethostbyname(socket.gethostname()) + "\nPort: " + str(port) + "\nStudentID: " + studentID + "\n")
-                #print threading.current_thread()
             if "JOIN_CHATROOM" in data:
                 client.send("JOINED_CHATROOM: room1\nSERVER_IP: " + socket.gethostbyname(socket.gethostname()) + "\nPORT: 0\nROOM_REF: 1\nJOIN_ID: 1\n")
             print("Result from request: %s" % (data))
             client.close()
 
     studentID = '677cfc77e52778a3d5741cb5d5f358c537c28f5134d63e4b7f8376f73315922c'
-    host = '' 
+    host = ''
     port = int(sys.argv[1])
     backlog = 5 
     size = 1024
@@ -41,8 +42,6 @@ if __name__ == '__main__':
     main = ThreadPool(threads)
     main.createWorkers(threads)
 
-    i = 0
-
     while True:
 
         if exit == 1:
@@ -52,7 +51,6 @@ if __name__ == '__main__':
             requests = makeRequests(use_client, sock)
             for req in requests:
                 main.putRequest(req)
-                #print("Work request #%s added." % req.requestID)
             time.sleep(0.5)
             main.poll()
         s.close
